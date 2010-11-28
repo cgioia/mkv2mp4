e02f0361eeaddb197743966749d7af0478d67926
Chad Gioia <cgioia@gmail.com>
2010-11-27 23:27:54 -0500

    Finished refactoring. Cleaned up.

--- ripsub.pl
+++ ripsub.pl
@@ -1,17 +1,24 @@
 #!/usr/bin/perl
+use strict;
 use warnings;
 use File::Basename;
 
-foreach ( @ARGV ) {
-   ($name, $path, $suffix) = fileparse( $_, qr/\.[^.]*/ );
-   if ( $suffix eq ".mkv" ) {
-      ($subname) = $name =~ /(?:\[.*?\])?([^\[\(]+)/;
-      foreach ( $subname ) { s/^[\s_]+//; s/[\s_]+$//; s/_/ /g; }
-      $info = `mkvmerge --identify "$_"`;
-      ($track) = $info =~ /Track ID (\d+): subtitles \(S_TEXT\/ASS\)/;
-      @args = ( "mkvextract", "tracks", $_, "$track:$subname.ass" );
-      #system( @args );
-      foreach (@args) { print "$_ "; }
-      print "\n";
+foreach ( @ARGV )
+{
+   # Get the name, path, and the suffix of the file.
+   my ($name, $path, $suffix) = fileparse( $_, qr/\.[^.]*/ );
+
+   # Only operate on Matroska video or subtitle containers.
+   if ( $suffix =~ /^\.mk[vs]$/ )
+   {
+      # Find the track ID for the ASS subtitles.
+      my $info = `mkvmerge --identify "$_"`;
+      my ($track) = $info =~ /Track ID (\d+): subtitles \(S_TEXT\/ASS\)/;
+
+      # Construct the system call to extract the subtitle track.
+      my @mkvargs = ( "mkvextract", "tracks", $_, "$track:$path$name.ass" );
+
+      # Make the system call.
+      system( @mkvargs );
    }
 }
