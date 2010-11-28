1a507a7427a6d91a8566e1e2b4824d0248a83da8
Chad Gioia <cgioia@gmail.com>
2010-11-27 22:55:14 -0500

    More modifications.

--- ripsub.pl
+++ ripsub.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl
+use warnings;
 use File::Basename;
 
 foreach ( @ARGV ) {
-   ($subname) = basename($_) =~ /(?:\[.*?\])?([^\[\(]+)/;
-   foreach ( $subname ) { s/^[\s_]+//; s/[\s_]+$//; s/_/ /g; }
-   #$info = `mkvmerge --identify "$_"`;
-   #($track) = $info =~ /Track ID (\d+): subtitles \(S_TEXT\/ASS\)/;
-   ($track) = `mkvmerge -i "$_"` =~ /Track ID (\d+): subtitles \(S_TEXT\/ASS\)/;
-   @args = ( "mkvextract", "tracks", $_, "$track:$subname.ass" );
-   #system( @args );
-   foreach (@args) { print "$_ "; }
-   print "\n";
+   ($name, $path, $suffix) = fileparse( $_, qr/\.[^.]*/ );
+   if ( $suffix eq ".mkv" ) {
+      ($subname) = $name =~ /(?:\[.*?\])?([^\[\(]+)/;
+      foreach ( $subname ) { s/^[\s_]+//; s/[\s_]+$//; s/_/ /g; }
+      $info = `mkvmerge --identify "$_"`;
+      ($track) = $info =~ /Track ID (\d+): subtitles \(S_TEXT\/ASS\)/;
+      @args = ( "mkvextract", "tracks", $_, "$track:$subname.ass" );
+      #system( @args );
+      foreach (@args) { print "$_ "; }
+      print "\n";
+   }
 }
